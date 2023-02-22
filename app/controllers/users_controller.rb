e10c88bb0d76ae465a46446c915f083087e908c2
austin-chen-booth <achen52@chicagobooth.edu>
2023-02-22 05:26:40 +0000

    Homework done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
+  def authenticate
+    # get the username from params
+    username = params.fetch("input_username")
+    # get the password from params
+    password = params.fetch("input_password")
+
+    # look up the record from the db matching usrname
+    user = User.where({ :username => username }).at(0)
+    # if there's no record, redirect to sign in form
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name 'round these parts." })
+    else
+      if user.authenticate(password)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back, #{user.username}" })
+      else
+        redirect_to("/user_sign_in", { :alert => "Nice try, sucker!" })
+      end
+    end
+  end
+
+  def new_registration_form
+    render({ :template => "users/signup_form.html.erb" })
+  end
+
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,21 +52,28 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, #{user.username}!" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
 
-
     user.username = params.fetch("input_username")
 
     user.save
-    
+
     redirect_to("/users/#{user.username}")
   end
 
@@ -42,5 +85,4 @@ class UsersController < ApplicationController
 
     redirect_to("/users")
   end
-
 end
